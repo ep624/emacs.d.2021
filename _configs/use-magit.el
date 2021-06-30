f1706acaa75b255f1ca4eb28f7f3ff86cccb341f
Vikas Rawal <vikasrawal@gmail.com>
2021-06-30 22:34:39 +0530

    Update

--- _configs/use-magit.el
+++ _configs/use-magit.el
@@ -5,14 +5,17 @@
 ;;
 ;;; Code:
 
-(require 'magit)
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-commit-arguments '("--verbose"))
+  (setq magit-repository-directories (quote (("~/.emacs.d" . 0) ("~/workspace/" . 1))))
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  (put 'magit-clean 'disabled nil)
+  (define-key magit-mode-map (kbd "C-c C") 'magit-clean)
+  )
+;;(require 'magit)
 
-(setq magit-commit-arguments '("--verbose"))
-(setq magit-repository-directories (quote (("~/.emacs.d" . 0) ("~/workspace/" . 1))))
-(setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-
-(put 'magit-clean 'disabled nil)
-(define-key magit-mode-map (kbd "C-c C") 'magit-clean)
 
 (provide 'use-magit)
 ;;; use-magit.el ends here
