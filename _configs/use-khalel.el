502a264a98b651b6756bc4c2eaedb5d7b45b7fa9
Vikas Rawal <vikasrawal@gmail.com>
2022-03-19 05:21:05 +0530

    Update before moving to org-ref 3.0

---
+++ _configs/use-khalel.el
@@ -0,0 +1,18 @@
+;; use-khalel
+
+(use-package khalel
+  :commands (khalel-export-org-subtree-to-calendar
+             khalel-import-upcoming-events
+             khalel-edit-calender-event
+             khalel-add-capture-template
+             ))
+
+(setq khalel-khal-command "/usr/bin/khal")
+(setq khalel-vdirsyncer-command "vdirsyncer")
+
+(setq khalel-capture-key "e")
+(setq khalel-import-org-file-confirm-overwrite nil)
+(setq khalel-import-time-delta "30d")
+(khalel-add-capture-template)
+
+(provide 'use-khalel)
