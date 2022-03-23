5a39162d3699bfb9a03b6ae1882b45475352e79e
Vikas Rawal <vikasrawal@gmail.com>
2022-03-23 07:19:32 +0530

    Working setup with citar

--- _configs/use-citar.el
+++ _configs/use-citar.el
@@ -15,18 +15,28 @@
 (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
 (setq citar-at-point-function 'embark-act)
 
+((prefix . "${:p} ${:n}")
+ (main . "${author editor:20}     ${date year issued:4}     ${title:30}")
+ (suffix . "          ${=key= id:15}   ${=type=:12}")
+ (preview . "${author editor} (${year issued date})  ${title}, ${journal journaltitle publisher container-title  collection-title}.\n")
+ (note . "Notes on ${author editor}, ${title}"))
+
+
 (setq citar-templates
       '((main . "${author editor:20}     ${date year issued:4}     ${title:30}")
         (suffix . "          ${=key= id:15}    ${=type=:12}")
         (preview . "${author editor} (${year issued date}) ${title}, ${journal journaltitle publisher container-title collection-title}.\n")
         (note . "Notes on ${author editor}, ${title}")))
 
-;; (use-package all-the-icons
-;;   :if (display-graphic-p)
-;;   :init
-;;   (setq citar-symbols
-;;       `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
-;;         (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
-;;         (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
-;;   (setq citar-symbol-separator "  ")
-;;   )
+
+;; install all-the-icons and run m-x all-the-icons-install-fonts for the following to work.
+
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :init
+  (setq citar-symbols
+      `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+        (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+        (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+  (setq citar-symbol-separator "  ")
+  )
